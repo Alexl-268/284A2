7d5602aea928b7ab8c6d47c6cd701e64259bffd9
Alexl-268 <zlia715@aucklanduni.ac.nz>
2021-08-11 15:05:47 +1200

    99% working

--- countoperation.py
+++ countoperation.py
@@ -4,18 +4,15 @@ def _mergeSort(arr, temp_arr, left, right):
 	if left < right:
 		mid = (left + right)//2
 
-		inv_count += _mergeSort(arr, temp_arr,
-									left, mid)
-
-		inv_count += _mergeSort(arr, temp_arr,
-								mid + 1, right)
+		inv_count += _mergeSort(arr, temp_arr, left, mid)
+		inv_count += _mergeSort(arr, temp_arr, mid + 1, right)
 		inv_count += merge(arr, temp_arr, left, mid, right)
 
 	return inv_count
 
 def merge(arr, temp_arr, left, mid, right):
 	i = left	 # Starting index of left subarray
-	j = mid + 1 # Starting index of right subarray
+	j = mid + 1  # Starting index of right subarray
 	k = left	 # Starting index of to be sorted subarray
 	inv_count = 0
 
@@ -43,44 +40,11 @@ def merge(arr, temp_arr, left, mid, right):
 
 	for loop_var in range(left, right + 1):
 		arr[loop_var] = temp_arr[loop_var]
-    
+
 	return inv_count
 
-def mergeSort(arr):
-	if len(arr) > 1:
-
-		mid = len(arr)//2
-
-		L = arr[:mid]
-
-		R = arr[mid:]
-
-		mergeSort(L)
-
-		mergeSort(R)
-
-		i = j = k = 0
-
-		while i < len(L) and j < len(R):
-			if L[i] < R[j]:
-				arr[k] = L[i]
-				i += 1
-			else:
-				arr[k] = R[j]
-				j += 1
-			k += 1
-
-		while i < len(L):
-			arr[k] = L[i]
-			i += 1
-			k += 1
-
-		while j < len(R):
-			arr[k] = R[j]
-			j += 1
-			k += 1
-
 scenario = input("numbers : ")
+
 
 def formattedArray():
 	userInput = input("input : ")
@@ -92,30 +56,33 @@ def formattedArray():
 
 	return userInput
 
+final = []
 scenario = int(scenario)
-while (scenario > 0 ):
+while (scenario > 0):
 	userInput = formattedArray()
 	if (userInput != ""):
 		int_list = [int(x) for x in userInput.split(",")]
 
-		dict =  {
+		dict = {
 		}
 
 		for i in range(1, len(int_list), 2):
 			dict[int_list[i-1]] = int_list[i]
 
-
-		arr = list(dict.keys())
-		n = len(arr)
-		temp_arr = [0]*n
-		mergeSort(arr)
+		keys = list(sorted(dict.keys()))
 
 		arrToCount = []
-		for i in arr :
+		for i in keys :
 			arrToCount.append(dict.get(i))
 
+		n= len(arrToCount)
+		temp_arr = [0]*n
 		result = _mergeSort(arrToCount, temp_arr, 0, n-1)
-		print(result)
+		final.append(result)
+	else:
+		final.append(0)
+	
+	scenario = scenario - 1
 
-	scenario = scenario -1
-
+for i in final:
+	print(i)
